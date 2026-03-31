a54896b574ef85dea0033bc5d6c8453d243d64a1
John Gitter Perito <brewmanwild@gmail.com>
2026-03-31 11:16:34 +0800

    Activity 1

---
+++ main/src/pages/home/Home.jsx
@@ -0,0 +1,17 @@
+
+import './Home.css'
+
+const Home = () => (
+    <div className="container">
+        <h2> ITE6-2A  Group6 </h2>
+        <p>This is a simple home page for the group.</p>
+        <ol>
+            <li>John Gitter Perito</li>
+            <li>Matthew Stephen Delda</li>
+            <li>Mary Annjellyn Laduiana</li>
+            <li>Ace Blixen Rubante</li>
+        </ol>
+    </div>
+)
+
+export default Home
